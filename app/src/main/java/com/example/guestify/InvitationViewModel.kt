eb948fbec5efbf70e6fda3561be039f73c73515a
Eden-Cohen1 <edencohen.dev@gmail.com>
2024-12-26 18:46:28 +0200

    added date&time picker dialog, added ViewModel for the invitation from data, saved the data to the ViewModel, added UI validation.

---
+++ app/src/main/java/com/example/guestify/InvitationViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.guestify
+
+import androidx.lifecycle.ViewModel
+
+
+data class InvitationData(
+    val groomName: String,
+    val brideName: String,
+    val eventDate: String,
+    val eventTime: String,
+    val eventLocation: String,
+    val invitationText: String
+)
+
+
+class InvitationViewModel : ViewModel() {
+
+    var invitationData : InvitationData ? = null
+        private set
+
+    fun submitInvitation(groomName: String,
+                         brideName: String,
+                         eventDate: String,
+                         eventTime: String,
+                         eventLocation: String,
+                         invitationText: String) {
+        this.invitationData = InvitationData(
+            groomName,
+            brideName,
+            eventDate,
+            eventTime,
+            eventLocation,
+            invitationText)
+    }
+
+}
