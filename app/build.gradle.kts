231fdce978cd95bd1e8eb1a73cd553f9ec57cc52
roydann <royid123@gmail.com>
2025-02-23 13:25:46 +0200

    geustfragment back button backstack action to dashboardfragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,5 +73,9 @@ dependencies {
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.android.gms:play-services-auth:20.7.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.google.android.libraries.places:places:3.4.0")
 
 }
